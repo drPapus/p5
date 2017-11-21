1a3e7bf2bb610d31c97298bee3d3b9f0837ef4d7
Klimov Anton <antonklimovv@gmail.com>
2017-11-21 14:36:26 +0200

    color

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
-function setup() {
-	createCanvas(400, 400);
-}
+ function setup() {
+	 createCanvas(640, 360);
+	 
+ }
 
-function draw() {
-	background(255,255,0);
+ function draw() {
+	background(255);
+	ellipseMode(CENTER);
+	rectMode(CENTER);
+
+
+	//body
+	fill(255, 0, 0)
+	rect(240, 145, 20, 100);
+	//head
 	
-}
+	ellipse(240, 115, 60, 60);
+	//eyes
+	fill(0, 255, 0)
+	ellipse(221, 115, 16, 32);
+	ellipse(259, 115, 16, 32);
+
+	//legs
+	line(230, 195, 220, 205);
+	line(250, 195, 260, 205);
+	
+ }
+
